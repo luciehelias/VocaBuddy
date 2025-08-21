3f93f5364e7b7556a743ee53d309da2695c92388
Lucie Hélias <lucie.helias@protonmail.com>
2025-08-21 10:34:24 +0200

    feat(favicon): add favicon to the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ import Footer from "@/components/layout/Footer";
 export const metadata: Metadata = {
   title: "VocaBuddy",
   description: "Buddy up your vocabulary with VocaBuddy!",
+  icons: {
+    icon: "/favicon.io",
+  },
 };
 
 export default function RootLayout({
