b4cda86d76018185fdde4fe5a78b2cf7a726cd65
Mathieu FONTAINE <mathieufontaine.dev@gmail.com>
2025-08-26 11:05:53 +0200

    fix getUser function

--- src/app/utils/getUser.ts
+++ src/app/utils/getUser.ts
@@ -1,24 +1,19 @@
 import { connectToDatabase } from "@/lib/dbconnect";
 import { currentUser } from "@clerk/nextjs/server";
-import { IUser, IUserDataResponse } from "@/types/user";
 import { User } from "@/models/User";
-
-const serializeUser = (user: IUserDataResponse): IUser => {
-  const newUser = {
-    ...user,
-    id: user._id.toString(),
-    createdAt: user.createdAt?.toISOString?.() ?? null,
-  };
-  return JSON.parse(JSON.stringify(newUser));
-};
 
 export const getUser = async () => {
   const clerkUser = await currentUser();
   if (!clerkUser) return null;
   
   await connectToDatabase();
-  const user = await User.findOne({ clerkId: clerkUser.id });
+  const user = await User.findOne({ clerkId: clerkUser.id }).lean();
   if (!user) return null;
 
-  return serializeUser(user as IUserDataResponse);
+    const newUser = {
+    ...user,
+    id: user._id.toString(),
+    createdAt: user.createdAt?.toISOString?.() ?? null,
+  };
+  return JSON.parse(JSON.stringify(newUser));
 };
