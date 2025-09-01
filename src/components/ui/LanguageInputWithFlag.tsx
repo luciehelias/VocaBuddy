54997c82ef995720e3ccc38ab50d82af97bd4c17
Lucie Hélias <lucie.helias@protonmail.com>
2025-09-01 17:15:03 +0200

    feat(feat/flashcard-input-flag): add languageInputWithFlag component to flashcardForm

---
+++ src/components/ui/LanguageInputWithFlag.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Input from "@/ui/Input";
+
+type LanguageInputWithFlagProps = {
+  languageCode: string;
+  languageName: string;
+  value: string;
+  onChange: (value: string) => void;
+};
+
+export default function LanguageInputWithFlag({
+  languageCode,
+  languageName,
+  value,
+  onChange,
+}: LanguageInputWithFlagProps) {
+  return (
+    <div className="flex gap-8 justify-between w-full items-center">
+      <Image
+        src={`/assets/flags/${languageCode}.png`}
+        alt={`Drapeau de ${languageName}`}
+        width={20}
+        height={20}
+        style={{ width: "32px", height: "32px", objectFit: "contain" }}
+        unoptimized
+      />
+      <Input
+        placeholder={`Mot en ${languageName}`}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
