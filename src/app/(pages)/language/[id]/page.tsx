b8f6cf81ebac887f13bf1b1d7d300ba6e38a307c
Mathieu FONTAINE <mathieufontaine.dev@gmail.com>
2025-09-01 10:24:47 +0200

    feat(language-menu): create language menu page and icon buttons

--- src/app/(pages)/language/[id]/page.tsx
+++ src/app/(pages)/language/[id]/page.tsx
@@ -1,36 +1,63 @@
 import Button from "@/components/ui/Button";
 import Title from "@/components/ui/Title";
 import { getLanguageName } from "@/utils/getLanguage";
+import { ArrowBigRight, BicepsFlexed, Dumbbell, Lightbulb } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function LanguageMenu({ params }: { params: { id: string } }) {
+export default async function LanguageMenu({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = await params;
   const language = getLanguageName(id);
-  
+
   if (!id || !language) return <div>No language found</div>;
 
   return (
-    <div className="flex flex-col gap-4 items-center justify-center">
-      <Title>Liste des actions pour la langue : {language}</Title>
-      <Image
-        src={`/assets/flags/${id}.png`}
-        alt={`Drapeau de ${language}`}
-        width={100}
-        height={100}
-      />
-      <ul className="flex gap-4">
+    <div className="flex flex-col gap-20 items-center justify-center">
+      <div className="flex justify-center gap-6 w-full">
+        <Image
+          src={`/assets/flags/${id}.png`}
+          alt={`Drapeau de ${language}`}
+          width={40}
+          height={40}
+        />
+        <Title>Hey! Prêt à travailler ton {language} ?</Title>
+      </div>
+      <ul className="flex gap-8">
         <li>
-          <Link href={`/flashcards/create/${id}`}>
-            <Button>Create Flashcards</Button>
+          <Link href={`/flashcards/${id}/create`}>
+            <Button variant="icon" className="create-color">
+              <Lightbulb size={64} color={"#FFD5A6"} />
+              <span className="font-bold">Mode Création</span>
+            </Button>
           </Link>
         </li>
         <li>
-          <Link href={`/flashcards/${id}`}>
-            <Button>All Flashcards</Button>
+          <Link href={`/language/${id}/train/`}>
+            <Button variant="icon" className="train-color">
+              <Dumbbell size={64} color={"#BFDBFF"} />
+              <span className="font-bold">Mode Entraînement</span>
+            </Button>
+          </Link>
+        </li>
+        <li>
+          <Link href={`/language/${id}/test`}>
+            <Button variant="icon" className="test-color">
+              <BicepsFlexed size={64} color={"#FFCCD3"} />
+              <span className="font-bold">Mode Révision</span>
+            </Button>
           </Link>
         </li>
       </ul>
+      <Link href={`/flashcards/${id}`}>
+        <Button className="flex items-center gap-2">
+          <ArrowBigRight />
+          <p>Gérer les flashcards</p>
+        </Button>
+      </Link>
     </div>
   );
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,5 +1,5 @@
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant?: "auth" | "primary" | "submit" | "option";
+  variant?: "auth" | "primary" | "submit" | "option" | "icon";
   className?: string;
 };
 
@@ -12,11 +12,12 @@ const Button = ({
   const baseStyle = "p-2 cursor-pointer";
 
   const variants = {
-    auth: "border-2 uppercase text-sm w-38 rounded-md",
-    primary: "border-2 uppercase rounded-xl",
+    auth: "border-2 uppercase text-sm w-38 rounded-md hover:bg-black hover:text-white",
+    primary: "border-2 uppercase rounded-xl hover:bg-emerald-200",
     submit:
       "border-1 bg-emerald-200 w-full uppercase rounded-xl hover:bg-emerald-300 ",
     option: "flex justify-center gap-2",
+    icon: "border-2 rounded-xl flex flex-col items-center gap-12 min-w-60 p-8 text-white",
   };
 
   return (
