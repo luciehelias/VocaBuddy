54f323bc94cfad6dfd9af8a139502c254d835fd2
Lucie Hélias <lucie.helias@protonmail.com>
2025-10-30 14:48:42 +0100

    feat(header-button-link): modify link in the header and create LinkButton component (#26)

--- src/app/(pages)/flashcards/[id]/create/page.tsx
+++ src/app/(pages)/flashcards/[id]/create/page.tsx
@@ -2,8 +2,7 @@ import Image from "next/image";
 import { getLanguageName } from "@/utils/language";
 import FlashcardForm from "@/components/form/FlashcardForm";
 import Title from "@/ui/Title";
-import Link from "next/link";
-import { ArrowBigRight } from "lucide-react";
+import LinkButton from "@/components/ui/LinkButton";
 
 export default async function FlashcardPage({
   params,
@@ -29,10 +28,10 @@ export default async function FlashcardPage({
       <div className="w-full flex justify-center">
         <FlashcardForm languageCode={id} learningLanguage={language} />
       </div>
-      <Link href={`/language/${id}`} className="hover:font-bold flex gap-4">
-        <ArrowBigRight />
-        <p>Je suis prêt à travailler mon {language} !</p>
-      </Link>
+      <LinkButton
+        href={`/language/${id}`}
+        text="Retour vers la page principale"
+      />
     </div>
   );
 }
